f4e2f67673a88842a6be2eff9830a6d1707fa3cb
deelight-del <akingbenidavidoyin@gmail.com>
2023-11-06 19:17:48 +0000

    playing around

---
+++ test_cmd.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import readline
+from cmd import Cmd
+
+class cmd_example(Cmd):
+    prompt = "(hbtn) "
+   # use_rawinput = True
+    def execute(self):
+        self.cmdloop()
+    def do_printf(self, cmd):
+        l_cmd = parse_arg(cmd)
+        actual_cmd = []
+        for chars in l_cmd:
+            if chars:
+                actual_cmd.append(chars)
+        for c in actual_cmd:
+            print(c)
+    def emptyline(self):
+        pass
+    def do_quit(self, arg):
+        return True
+    def complete_foobar(self, text, line, begidx, endidx):
+        pass
+    def do_EOF(self, line):
+        print()
+        return True
+    def precmd(self, line):
+      return line
+
+def parse_arg(str_cmd):
+    l_cmd = []
+    characters = ""
+    within_quotes = False
+    for char in str_cmd:
+        if char == "\"" or within_quotes:
+            if char == "\"" and within_quotes:
+                within_quotes = False
+                l_cmd.append(characters)
+                characters = ""
+                continue
+            elif char == "\"":
+                within_quotes = True
+                continue
+            else:
+                characters += char
+        elif not within_quotes and (char == " " or char == "\n"):
+            l_cmd.append(characters)
+            characters = ""
+        else:
+            characters = characters + char
+    #l_cmd = str_cmd.split(" ")
+    return l_cmd
+
+if __name__ == "__main__":
+    shell = cmd_example()
+    shell.execute()
+    #lst = parse_arg('ll\n')
+    #print(lst)
